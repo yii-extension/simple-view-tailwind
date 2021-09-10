3ca1e338c21eb1a909af65f681061d69b5f5e7f5
Wilmer <terabytesoftw@gmail.com>
2021-09-10 06:42:37 -0300

    Fix built-in server routing.

--- tests/_data/public/index.php
+++ tests/_data/public/index.php
@@ -15,9 +15,7 @@ if (is_file($c3)) {
 // PHP built-in server routing.
 if (PHP_SAPI === 'cli-server') {
     // Serve static files as is.
-    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-    if (is_file(__DIR__ . $path)) {
+    if (is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
         return false;
     }
 
